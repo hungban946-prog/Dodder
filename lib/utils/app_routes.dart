42ea55e86b6fd50a0a9a91570c737ad33833242b
hungban946-prog <hungban946@gmail.com>
2026-07-13 10:43:41 +0700

    Add files via upload

---
+++ lib/utils/app_routes.dart
@@ -0,0 +1,7 @@
+class AppRoutes {
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String profile = '/profile';
+  static const String pairing = '/pairing';
+  static const String home = '/home';
+}
